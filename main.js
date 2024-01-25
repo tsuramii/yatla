a87f540f1f4c80c37700fb6e3adef1516cc87bb7
Luca Di Marco <lucanahtsu@gmail.com>
2024-01-24 22:58:08 -0300

    Refactor date and task logic

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const days = [
   "Friday",
   "Saturday",
 ];
-
 const months = [
   "January",
   "February",
@@ -23,49 +22,40 @@ const months = [
   "December",
 ];
 
-// Get HTML elements
 const dateElement = document.querySelector("#date");
 const addTaskElement = document.querySelector("#addTask");
 const taskListElement = document.querySelector("#taskList");
 
-// Get date
 const currentDate = new Date();
-const month = months[currentDate.getMonth()];
-const dayName = days[currentDate.getDay()];
-const day = currentDate.getUTCDate();
+const [month, dayName, day] = [
+  months[currentDate.getMonth()],
+  days[currentDate.getDay()],
+  currentDate.getUTCDate(),
+];
 
-// Display date
 dateElement.innerHTML = `${dayName}, ${month} ${day}`;
 
 addTaskElement.addEventListener("submit", (event) => {
   event.preventDefault();
-  let value = addTaskElement.querySelector("input").value;
-  if (value) {
-    addTask(value);
-  }
+  const value = addTaskElement.querySelector("input").value;
+  value && addTask(value);
 });
 
-// Tasks logic with localStorage
-let tasks = { ...localStorage };
+const tasks = { ...localStorage };
 let taskCount = localStorage.length;
 
-for (let [taskId, taskValue] of Object.entries(tasks)) {
-  let taskItem = `
-  <li id="${taskId}" class="w-full shadow-lg my-1 bg-slate-200 hover:bg-slate-100 transition-colors text-black/90 p-3 rounded-md"><button class="-mb-2" onclick="deleteTask(${taskId})"><ion-icon name="ellipse-outline"></ion-icon></button> ${taskValue}</li>
-  `;
-  taskListElement.innerHTML += taskItem;
+for (const [taskId, taskValue] of Object.entries(tasks)) {
+  taskListElement.innerHTML += `<li id="${taskId}" class="w-full shadow-lg my-1 bg-slate-200 hover:bg-slate-100 transition-colors text-black/90 p-3 rounded-md"><button class="-mb-2" onclick="deleteTask(${taskId})"><ion-icon name="ellipse-outline"></ion-icon></button> ${taskValue}</li>`;
 }
+
 function addTask(task) {
-  let taskItem = `
-  <li id="${taskCount}" class="w-full shadow-lg my-1 bg-slate-200 hover:bg-slate-100 transition-colors text-black/90 p-3 rounded-md"><button class="-mb-2" onclick="deleteTask(${taskCount})"><ion-icon name="ellipse-outline"></ion-icon></button> ${task}</li>
-  `;
-  taskListElement.innerHTML += taskItem;
+  taskListElement.innerHTML += `<li id="${taskCount}" class="w-full shadow-lg my-1 bg-slate-200 hover:bg-slate-100 transition-colors text-black/90 p-3 rounded-md"><button class="-mb-2" onclick="deleteTask(${taskCount})"><ion-icon name="ellipse-outline"></ion-icon></button> ${task}</li>`;
   localStorage.setItem(taskCount, task);
   taskCount++;
 }
 
 function deleteTask(taskId) {
-  let taskItem = document.getElementById(taskId);
+  const taskItem = document.getElementById(taskId);
   localStorage.removeItem(taskId);
   taskItem.remove();
 }
